b15c0f84e2a72fd47a464e337004eec3f34733fc
BlueWay32Dev <130433209+BlueWay32Dev@users.noreply.github.com>
2025-08-21 12:37:19 +1000

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   css: ["~/assets/scss/main.scss"],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3000/api",
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://pruebatecnicanicolasroacajalosheores.onrender.com/api",
     },
   },
   vite: {
